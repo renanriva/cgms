381a7f5ca1a1fc8a79ae3a12056d95d13c38e80e
Ariful Haque <ariful.haque@icarasia.com>
2018-06-07 01:00:07 +0800

    search working

--- app/Http/Controllers/PortfolioController.php
+++ app/Http/Controllers/PortfolioController.php
@@ -17,20 +17,60 @@ class PortfolioController extends Controller
 
     /**
      * @todo add guard policy
+     * @param Request $request
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
      */
-    public function teachers(){
+    public function teachers(Request $request){
 
 
         $user = Auth::user();
 //        if ($user->can('browse', Teacher::class)) {
 
-
-
             if ($user->role == 'admin'){
 
+
+                $search_in = $request->input('search_param');
+                $search_keyword = $request->input('x');
+                $registration = $request->input('registration') == null ? 3 : $request->input('registration');
+
+
                 $title = 'Teacher Portfolio - '.env('APP_NAME') ;
-                $registrations = Registration::orderBy('id', 'desc')
-                                        ->paginate(10);
+
+                $registrations = Registration::where(function ($query) use($search_in, $search_keyword, $registration){
+
+
+
+                    if($registration !== 3){
+                        if ($registration == 1 || $registration == 0){
+                            $query->where('is_approved', $registration);
+                        }
+                    }
+
+                    if ($search_in == 'teachers_name'){
+                        // teacher name search
+
+                        $query->whereHas('student', function ($cQuery) use ($search_keyword){
+                            $cQuery->where('first_name', 'LIKE', '%' . $search_keyword . '%')
+                            ->orWhere('last_name', 'LIKE', '%'.$search_keyword.'%');
+                        });
+
+                    } elseif ($search_in == 'course_name'){
+
+                        $query->whereHas('course', function ($cQuery) use ($search_keyword){
+                            $cQuery->where('short_name', 'LIKE', '%' . $search_keyword . '%');
+                        });
+
+                    } elseif ($search_in == 'course_code'){
+
+                        $query->whereHas('course', function ($cQuery) use ($search_keyword){
+                            $cQuery->where('course_code',  $search_keyword );
+                        });
+
+                    }
+
+                })->orderBy('id', 'desc')
+                ->paginate(10);
+
             } elseif ($user->role == 'teacher'){
 
                 $title = 'My Portfolio - '.env('APP_NAME') ;
